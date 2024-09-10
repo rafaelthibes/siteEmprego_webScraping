3c52ad8dbdd9d2516f3400b69c55130c0aa4f451
Rafael <DE0HBFH@vw.vwgroup.com>
2024-09-10 16:24:20 -0300

    update

--- site_emprego_webscraping.py
+++ site_emprego_webscraping.py
@@ -1,13 +1,13 @@
 import bs4
 import requests
 
-url = requests.get('https://www.infojobs.com.br/empregos.aspx?palabra=Desenvolvedor')
+url = requests.get('https://www.infojobs.com.br/empregos.aspx?palabra=Desenvolvedor') #alterar ultima palavra do link para analisar outras vagas
 soup = bs4.BeautifulSoup(url.text,"html.parser")
 soup_links = soup.find_all('a',class_='text-decoration-none')
-print(len(soup_links))
+print(len(soup_links)) #analisando quantos links existe
 
+#Looping para captura das primeiras vagas disponibilizadas pelo site
 for link in soup_links:
-
     with open('empregos.txt', 'a') as empregos:
         link1 = link.text
         link2 = link.get('href')
